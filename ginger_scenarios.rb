e00a568cd05b25cd7c70649b94ece0a7fa26c075
Myron Marston <myron.marston@gmail.com>
2010-03-14 22:12:12 -0700

    Rails didn't run on ruby 1.9 until 2.3, so we don't want to test pre-2.3 versions of ActiveRecord against ruby 1.9.

--- ginger_scenarios.rb
+++ ginger_scenarios.rb
@@ -11,12 +11,13 @@ Ginger.configure do |config|
   config.aliases["active_record"] = "activerecord"
   config.aliases["active_support"] = "activesupport"
 
-  %w(
-  2.3.5 2.3.4 2.3.3 2.3.2
-  2.2.3 2.2.2
-  2.1.2 2.1.1 2.1.0
-  2.0.5 2.0.4 2.0.2 2.0.1 2.0.0
-  ).each do |version|
+  versions = %w( 2.3.5 2.3.4 2.3.3 2.3.2 )
+  versions += %w(
+    2.2.3 2.2.2
+    2.1.2 2.1.1 2.1.0
+    2.0.5 2.0.4 2.0.2 2.0.1 2.0.0
+  ) if RUBY_VERSION =~ /^1\.8/
+  versions.each do |version|
     config.scenarios << create_scenario(version)
   end
 end
