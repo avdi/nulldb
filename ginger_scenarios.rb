42a4bc5a238573d5e57d8ab3065881a1dec3bd9a
Myron Marston <myron.marston@gmail.com>
2010-08-24 07:37:06 -0700

    Test activerecord rc2 instead of beta4.

--- ginger_scenarios.rb
+++ ginger_scenarios.rb
@@ -13,8 +13,8 @@ Ginger.configure do |config|
 
   versions = []
 
-  # Rails 3 only works on Ruby 1.8.7
-  versions << '3.0.0.beta4' if RUBY_VERSION == '1.8.7'
+  # Rails 3 only works on Ruby 1.8.7 and 1.9.2
+  versions << '3.0.0.rc2' if %w[1.8.7 1.9.2].include?(RUBY_VERSION)
   versions += %w( 2.3.8 2.3.5 2.3.4 2.3.3 2.3.2 )
   versions += %w(
     2.2.3 2.2.2
