082c096062355b680684cc167b4031aa26b62675
Myron Marston <myron.marston@gmail.com>
2010-08-24 20:58:15 -0700

    Ensure ginger loads the right version of active record when "active_record/version" is required.

--- ginger_scenarios.rb
+++ ginger_scenarios.rb
@@ -9,6 +9,7 @@ end
 
 Ginger.configure do |config|
   config.aliases["active_record"] = "activerecord"
+  config.aliases["active_record/version"] = "activerecord"
   config.aliases["active_support"] = "activesupport"
 
   versions = []
