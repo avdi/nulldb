4f972ecd945dd370ed0cb21c47ab9915ef48957c
Marcel Pursche <marcel.pursche@3dcontentlogistics.com>
2019-08-26 14:30:18 +0200

    Add change column support

--- lib/active_record/connection_adapters/nulldb_adapter/core.rb
+++ lib/active_record/connection_adapters/nulldb_adapter/core.rb
@@ -235,6 +235,13 @@ class ActiveRecord::ConnectionAdapters::NullDBAdapter < ActiveRecord::Connection
     columns(table_name).detect { |col| col.sql_type == :primary_key }.try(:name)
   end
 
+  def change_column(table_name, column_name, type, options = {})
+    table_meta = @tables[table_name]
+    column = table_meta.columns.find { |column| column.name == column_name.to_s }
+    column.type = type
+    column.options = options if options
+  end
+
   protected
 
   def select(statement, name = nil, binds = [])
--- spec/nulldb_spec.rb
+++ spec/nulldb_spec.rb
@@ -368,6 +368,44 @@ describe 'rename_table' do
   end
 end
 
+describe 'change_column' do
+  before(:all) do
+    ActiveRecord::Base.establish_connection :adapter => :nulldb
+    ActiveRecord::Migration.verbose = false
+
+    ActiveRecord::Schema.define do
+      create_table(:employees) do |t|
+        t.string  :name, null: false, limit: 50
+        t.date    :hire_date
+        t.integer :employee_number
+        t.decimal :salary
+      end
+    end
+  end
+
+  def should_have_column(klass, col_name, col_type)
+    col = klass.columns_hash[col_name.to_s]
+    expect(col.sql_type.to_s.gsub(/\([0-9]+\)/, "").to_sym).to eq col_type
+  end
+
+  it 'should change the column type' do
+    expect{
+      ActiveRecord::Schema.define do
+        change_column :employees, :name, :text
+      end
+      Employee.connection.schema_cache.clear!
+      Employee.reset_column_information
+    }.to_not raise_error
+
+    columns = Employee.columns
+
+    should_have_column(Employee, :name, :text)
+    should_have_column(Employee, :hire_date, :date)
+    should_have_column(Employee, :employee_number, :integer)
+    should_have_column(Employee, :salary, :decimal)  
+  end
+end
+
 describe 'adapter-specific extensions' do
   before(:all) do
     ActiveRecord::Base.establish_connection :adapter => :nulldb
